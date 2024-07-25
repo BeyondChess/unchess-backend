7d4df12a76f67848fba94bb312aa4eb4afe7e60e
substantialcattle5 <nilaynathsharan16@gmail.com>
2024-07-25 05:42:18 +0530

    add: service files && fix: ci

---
+++ apps/api-gateway/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"api-gateway/internal/server"
+	"fmt"
+)
+
+func main() {
+
+	server := server.NewServer()
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
---
+++ apps/notification-service/internal/server/routes.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (s *FiberServer) RegisterFiberRoutes() {
+	s.App.Get("/", s.HelloWorldHandler)
+
+}
+
+func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
+	resp := fiber.Map{
+		"message": "Hello World",
+	}
+
+	return c.JSON(resp)
+}
